bb4c9865f4b2227902333946550c61ccaed9e32d
looken1024 <looken1024@126.com>
2025-05-04 23:13:22 +0800

    feat:位运算

---
+++ 数组/57 - II. 和为s的连续正数序列.cc
@@ -0,0 +1,23 @@
+/*
+题目描述:输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+
+来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof
+
+解题思路：双指针
+
+时间复杂度：O(N)
+
+空间复杂度：O(1)
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> get_lists
+
+int main() {
+    
+    return 0;
+}
