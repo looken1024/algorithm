afa8269161b729cea2765c321187e6440d80d653
looken1024 <looken1024@126.com>
2025-05-10 17:28:45 +0800

    feat:设计模式-单例

--- 设计模式/单例/静态内部类实现-线程安全.java
+++ 设计模式/单例/静态内部类实现-线程安全.java
@@ -7,7 +7,7 @@ public class Singleton {
 
     /*
     当 Singleton 类被加载时，静态内部类 SingletonHolder 没有被加载进内存。
-    只有当调用 getUniqueInstance() 方法从而触发 SingletonHolder.INSTANCE 时
+    只有当调用 getInstance() 方法从而触发 SingletonHolder.INSTANCE 时
     SingletonHolder 才会被加载，
     此时初始化 INSTANCE 实例，
     并且 JVM 能确保 INSTANCE 只被实例化一次。
--- 设计模式/单例/饿汉式-线程安全2.java
+++ 设计模式/单例/饿汉式-线程安全2.java
@@ -9,7 +9,11 @@ public class Singleton {
      * 缺点：由于在类加载的时候就实例化了，所以没有达到Lazy Loading(懒加载)的效果，也就是说可能我没有用到这个实例，但是它
      * 也会加载，会造成内存的浪费(但是这个浪费可以忽略，所以这种方式也是推荐使用的)。
     */
-    private static final Singleton instance = new Singleton();
+    private static final Singleton instance = null;
+
+    static {
+        instance = new Singleton();
+    }
 
     private Singleton() {}
 
